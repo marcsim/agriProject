472f2b60186dd585e6ad720f7dc45f37d08204df
marcsim <marc-antoine.simon3@orange.fr>
2021-04-28 20:37:12 +0200

    implement firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import firebase from 'firebase/app';
+import 'firebase/firestore';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,19 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor() {}
+  constructor() {
+    const firebaseConfig = {
+      apiKey: 'AIzaSyAgqf4p4Hn7ScOhvxGsudvCrYZ05elfZV0',
+      authDomain: 'agriproject-df5f4.firebaseapp.com',
+      databaseURL: 'https://agriproject-df5f4-default-rtdb.europe-west1.firebasedatabase.app',
+      projectId: 'agriproject-df5f4',
+      storageBucket: 'agriproject-df5f4.appspot.com',
+      messagingSenderId: '807796952573',
+      appId: '1:807796952573:web:929f82b278919eb0989d94',
+      measurementId: 'G-Q31TXT0BWF'
+    };
+
+    firebase.initializeApp(firebaseConfig);
+    firebase.analytics();
+  }
 }
